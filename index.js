3cc4c02fc718aa37513617adc613168a81b4416a
Rob van Groenewoud <robvangroenewoud@gmail.com>
2016-12-07 22:20:59 +0100

    Added static landing page

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
 var port = process.env.PORT || 8080;
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
